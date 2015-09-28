d3929f7b9bb153c7ed1b8d6ce2f1872db4da7942
John Chapman <JayC202@gmail.com>
2015-09-27 20:06:48 -0400

    Added src items to index for exposing from the main module

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 import Dispatcher from './src/Dispatcher';
+import DisposableStack from './src/DisposableStack';
+import PureView from './src/PureView';
+import Store from './src/Store';
+import View from './src/View';
 
 export default {
-	Dispatcher: Dispatcher
+	Dispatcher,
+	DisposableStack,
+	PureView,
+	Store,
+	View
 };
